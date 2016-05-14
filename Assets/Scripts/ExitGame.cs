1fa345e2d10ecb7a17420f29d9a97730459272f6
James Reilman <James Reilman>
2016-05-13 23:48:30 -0700

    Add New Game and Exit Game Options

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+
+	public void ExitClicked(){
+		Application.Quit ();
+	}
+
+	void Update() {
+		if (Input.GetKey ("escape"))
+			Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/ReloadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReloadOnClick : MonoBehaviour {
+
+	public void ReloadClicked(){
+		SceneManager.LoadScene("Main");
+	}
+}
